62ebcbb98026e3a3abd94a7a11fd2a1f3c0fc06c
ismap <iselpuro@hotmail.com>
2017-09-20 22:07:37 -0700

    minor change

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -1,10 +1,7 @@
 class CallbackController < ApplicationController
   def index
   	if params["hub.verify_token"] == "fbToken"
-  		#render text: params["hub.challenge"]
-  		render text: "notmatch"
-  	else
-  		render text: "Unmatch"
+  		render text: params["hub.challenge"]
   	end
   end
 
